33c1ed52c9331b08fc15692cfad2e9a28fe20612
abbhanda@g.syr.edu <abbhanda@g.syr.edu>
2025-11-22 20:04:01 +0000

    .

---
+++ northwind/models/dim_product.sql
@@ -0,0 +1,13 @@
+with stg_products as (
+    select * from {{ source('northwind','Products')}}
+),
+stg_categories as (
+    select * from {{ source('northwind','Categories')}}
+)
+select 
+    {{ dbt_utils.generate_surrogate_key(['p.productid']) }} as productkey,
+    p.*,
+    c.categoryname,
+    c.description as categorydescription
+from stg_products p
+    left join stg_categories c on p.categoryid = c.categoryid
