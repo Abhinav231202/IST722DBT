50d9cbf6e9dc8ad1905cf07065700fbf5e8765f3
Abhinav Bhandatri <abbhanda@g.syr.edu>
2025-11-30 18:33:58 +0000

    .

--- northwind/models/staging/stg_uber_trips.sql
+++ northwind/models/staging/stg_uber_trips.sql
@@ -1,9 +1,17 @@
 -- This file pulls data from Snowflake
 SELECT 
     trip_id,
-    date_key,
-    fare_amount,
+    pickup_datetime,
+    pickup_date,
+    pickup_hour,
+    day_name,
+    day_type,
+    pickup_longitude,
+    pickup_latitude,
+    dropoff_longitude,
+    dropoff_latitude,
     trip_distance_miles,
-    passenger_count
-FROM {{ source('uber_snowflake', 'FACT_TRIPS') }}
-LIMIT 1000
+    passenger_count,
+    fare_amount,
+    payment_type
+FROM {{ source('uber_snowflake', 'RAW_UBER_TRIPS') }}
